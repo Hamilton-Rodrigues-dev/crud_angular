baaabdf7ff913d798668910642257d5ace3ec345
Hamilton <hamilton.rodrigues.teixeira@gmail.com>
2025-05-13 19:22:37 -0300

    feat(courses): listagem dos cursos

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses.component';
 import { MatTableModule } from '@angular/material/table';
+import { MaterialImportsModule } from '../shared/material.imports/material.imports.module';
 
 @NgModule({
   declarations: [CoursesComponent],
-  imports: [CommonModule, CoursesRoutingModule, MatTableModule],
+  imports: [CommonModule, CoursesRoutingModule, MaterialImportsModule],
 })
 export class CoursesModule {}
